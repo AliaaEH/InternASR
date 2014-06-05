d82e593bfb1ee90cf4be274cbb34ce4d17732eb4
AliaaEH <aliaaessam95@hotmail.com>
2014-06-05 14:42:30 +0300

    code cleaned

--- First Prototype/Second Prototype/ViewController.m
+++ First Prototype/Second Prototype/ViewController.m
@@ -19,12 +19,12 @@ void AQInputCallback(void * inUserData,  // Custom audio metadata
 
 @implementation ViewController
 
+#pragma mark - UIViewController
+
 - (void)viewDidLoad
 {
-    [super viewDidLoad];
-    
+    //load background picture
     self.view.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"bg.jpg"]];
-	// Do any additional setup after loading the view, typically from a nib.
     
     //setting up display text field
     recognizedText.editable = NO;
@@ -37,10 +37,10 @@ void AQInputCallback(void * inUserData,  // Custom audio metadata
     //initiating flags
     dictating = NO;
     
+    //initializing Audio session
     NSError *audioSessionError;
     AVAudioSession *audioSession = [AVAudioSession sharedInstance];
     [audioSession setCategory:AVAudioSessionCategoryPlayAndRecord error:&audioSessionError];
-    
     if(audioSessionError)
     {
         NSLog(@"AVAudioSession error setting category:%@",audioSessionError);
@@ -51,133 +51,24 @@ void AQInputCallback(void * inUserData,  // Custom audio metadata
         if(audioSessionError)
             NSLog(@"AVAudioSession error activating: %@",audioSessionError);
     }
+    
+    //load view
+    [super viewDidLoad];
 }
 
--(int) AQmain
+- (void)didReceiveMemoryWarning
 {
-    AudioStreamBasicDescription recordFormat;
-    memset(&recordFormat, 0, sizeof(recordFormat));
-    
-    recordFormat.mFormatID = kAudioFormatLinearPCM;
-    recordFormat.mChannelsPerFrame = 1;
-    recordFormat.mSampleRate = 16000;
-    recordFormat.mChannelsPerFrame = 1;
-    recordFormat.mFormatFlags = kAudioFormatFlagIsSignedInteger;
-    
-    recordFormat.mFormatFlags        = kAudioFormatFlagIsSignedInteger | kAudioFormatFlagIsPacked;
-    recordFormat.mFramesPerPacket    = 1;
-    recordFormat.mBitsPerChannel     = 16;
-    recordFormat.mBytesPerPacket     = 2;
-    recordFormat.mBytesPerFrame      = 2;
-    
-    
-    int bufferByteSize = 1024;
-    UInt32 propSize = sizeof(recordFormat);
-    
-    AudioFormatGetProperty(kAudioFormatProperty_FormatInfo, 0, NULL, &propSize, &recordFormat);
-    
-    //queue created
-    AudioQueueNewInput(&recordFormat,
-                       AQInputCallback,
-                       &recorder,
-                       CFRunLoopGetCurrent(),
-                       kCFRunLoopCommonModes,
-                       0,
-                       &queue);
-    
-    UInt32 size = sizeof(recordFormat);
-    AudioQueueGetProperty(queue,
-                          kAudioConverterCurrentOutputStreamDescription,
-                          &recordFormat,
-                          &size);
-    
-    //create buffers and prepare queue
-    int bufferIndex;
-    for (bufferIndex = 0; bufferIndex < NUM_BUFFERS; ++bufferIndex)
-    {
-        AudioQueueBufferRef buffer;
-        
-        AudioQueueAllocateBuffer(queue,
-                                 bufferByteSize,
-                                 &buffer);
-        
-        AudioQueueEnqueueBuffer(queue,
-                                buffer,
-                                0,
-                                NULL);
-    }
-    
-    recorder.stream = (__bridge void *)(outputStream);
-    recorder.queue = queue;
-    
-    return 1;
-    
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
 }
 
+#pragma mark - IBActions
 
-//setting up callback function
-void AQInputCallback(void * inUserData,
-                        AudioQueueRef inAQ,
-                        AudioQueueBufferRef inBuffer,
-                        const AudioTimeStamp * inStartTime,
-                        UInt32 inNumberPacketDescriptions,
-                        const AudioStreamPacketDescription * inPacketDescs)
+-(IBAction)clearText:(id)sender
 {
-    
-    recorderx *recorder = (recorderx*)inUserData;
-    
-    void *pointer = recorder->stream;
-    NSOutputStream *outputstream = (__bridge NSOutputStream*)pointer;
-    
-    //extracting recording size
-    NSData *data_body = [[NSData alloc] initWithBytes:inBuffer->mAudioData length:inBuffer->mAudioDataByteSize];
-    
-    //convert int to NSData
-    int i = [data_body length];
-    if (i%2 != 0) //size must be even (server expectation)
-    {
-        i--;
-    }
-    
-    NSData *size = [NSData dataWithBytes: &i length: sizeof(i)];
-    
-    //creating and sending packet
-    NSMutableData *packet = [[NSMutableData alloc] init];
-    [packet appendData:size];
-    [packet appendData:data_body];
-    
-    if (i>0)
-    {
-        [outputstream write:[packet bytes] maxLength:[packet length]];
-    }
-    
-    AudioQueueEnqueueBuffer(recorder->queue, inBuffer, 0, NULL);
+    recognizedText.text = @"No recognized Text Yet";
 }
 
-//Two main profram functions
--(void)start
-{
-    NSLog(@"Starting");
-    dictating = YES;
-    
-    [self initNetworkCommunication];
-    [self AQmain];
-    
-    recorder.running = YES;
-    AudioQueueStart(queue, NULL);
-    
-}
-
--(void)stop
-{
-    NSLog(@"Stopping");
-    dictating = NO;
-    
-    recorder.running = NO;
-    AudioQueueStop(queue, YES);
-}
-
-//Defining main button behavior
 -(IBAction)startDictation:(id)sender
 {
     if (dictating)      //the button should act as a pause button
@@ -195,8 +86,121 @@ void AQInputCallback(void * inUserData,
     
 }
 
-//------------------------------ SET-UP STREAM ---------------------------//
+#pragma mark - Controlling Dictator
 
+-(void)start
+{
+    
+    dictating = YES;                    //adjust flag
+    [self initNetworkCommunication];    //establish a TCP connection with the server
+    [self AQmain];                      //start a new AudioQueue
+    
+    recorder.running = YES;             //adjust UserData's flag
+    AudioQueueStart(queue, NULL);
+    
+}
+
+-(void)stop
+{
+    dictating = NO;                     //adjust flag
+    [outputStream close];               //tear up previous TCP streams
+    [inputStream close];
+    recorder.running = NO;              //adjust UserData's flag
+    AudioQueueStop(queue, YES);
+}
+
+#pragma mark - AudioQueue
+
+-(void) AQmain
+{
+    //setting up the audio's format
+    AudioStreamBasicDescription recordFormat;
+    memset(&recordFormat, 0, sizeof(recordFormat));
+    
+    recordFormat.mFormatID = kAudioFormatLinearPCM;
+    recordFormat.mChannelsPerFrame = 1;
+    recordFormat.mSampleRate = 16000;
+    recordFormat.mChannelsPerFrame = 1;
+    recordFormat.mFormatFlags = kAudioFormatFlagIsSignedInteger;
+    
+    recordFormat.mFormatFlags        = kAudioFormatFlagIsSignedInteger | kAudioFormatFlagIsPacked;
+    recordFormat.mFramesPerPacket    = 1;
+    recordFormat.mBitsPerChannel     = 16;
+    recordFormat.mBytesPerPacket     = 2;
+    recordFormat.mBytesPerFrame      = 2;
+    
+    
+    //setting up queue
+    AudioQueueNewInput(&recordFormat,
+                       AQInputCallback,
+                       &recorder,
+                       CFRunLoopGetCurrent(),
+                       kCFRunLoopCommonModes,
+                       0,
+                       &queue);
+    
+    
+    //creating buffers in the queue
+    int bufferByteSize = 1024;
+    int bufferIndex;
+    for (bufferIndex = 0; bufferIndex < NUM_BUFFERS; ++bufferIndex)
+    {
+        AudioQueueBufferRef buffer;
+        
+        AudioQueueAllocateBuffer(queue,
+                                 bufferByteSize,
+                                 &buffer);
+        
+        AudioQueueEnqueueBuffer(queue,
+                                buffer,
+                                0,
+                                NULL);
+    }
+    
+    //Saving View Controller's variables in the recorder struct
+    recorder.stream = (__bridge void *)(outputStream);
+    recorder.queue = queue;
+}
+
+//buffer call back function - called whenever a buffer is full
+void AQInputCallback(void * inUserData,
+                        AudioQueueRef inAQ,
+                        AudioQueueBufferRef inBuffer,
+                        const AudioTimeStamp * inStartTime,
+                        UInt32 inNumberPacketDescriptions,
+                        const AudioStreamPacketDescription * inPacketDescs)
+{
+    //extract the data in the inUserData struct
+    recorderx *recorder = (recorderx*)inUserData;
+    NSOutputStream *outputstream = (__bridge NSOutputStream*)recorder->stream;
+    
+    //creating the packet to be sent
+    //data
+    NSData *data_body = [[NSData alloc] initWithBytes:inBuffer->mAudioData length:inBuffer->mAudioDataByteSize];
+    
+    //data size
+    int i = [data_body length];
+    if (i%2 != 0) //size must be even (server expectation)
+    {
+        i--;
+    }
+    NSData *size = [NSData dataWithBytes: &i length: sizeof(i)];
+    
+    //appending to one object
+    NSMutableData *packet = [[NSMutableData alloc] init];
+    [packet appendData:size];
+    [packet appendData:data_body];
+    
+    if (i>0)    //only send buffers with data
+    {
+        [outputstream write:[packet bytes] maxLength:[packet length]];
+    }
+    
+    //return the buffer to the end of the queue
+    AudioQueueEnqueueBuffer(recorder->queue, inBuffer, 0, NULL);
+}
+
+#pragma mark - SocketConnection
 - (void)initNetworkCommunication
 {
     CFReadStreamRef readStream;
@@ -275,11 +279,10 @@ void AQInputCallback(void * inUserData,
 	}
     
 }
-// -------------------------------------//
 
 -(void)displayText:(NSString *)reply
 {
-    //start text recognition
+    //start text recognition string
     if ([recognizedText.text isEqualToString: @"No recognized Text Yet"])
     {
         recognizedText.text = @"";
@@ -297,17 +300,6 @@ void AQInputCallback(void * inUserData,
     
     //displaying new text
     recognizedText.text = [NSString stringWithFormat:@"%@ %@", recognizedText.text, input];
-}
-
--(IBAction)clearText:(id)sender
-{
-    recognizedText.text = @"No recognized Text Yet";
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 @end
